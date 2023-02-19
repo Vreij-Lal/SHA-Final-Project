45d7b57106de2e1b411e646e81688004930b50ae
vreij-lal <vreijlal@gmail.com>
2023-02-19 03:30:42 +0200

    additional components added

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import Homepage from "./components/Homepage";
 import UserHomepage from "./components/UserHomepage";
+
+import Explore from "./components/Explore";
+import Messages from "./components/Messages";
+import Profile from "./components/Profile";
+import More from "./components/More";
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />}></Route>
       <Route path="/homepage" element={<UserHomepage />}></Route>
-      <Route path="explore" element={<UserHomepage />}></Route>
-      <Route path="messages" element={<UserHomepage />}></Route>
-      <Route path="profile" element={<UserHomepage />}></Route>
-      <Route path="more" element={<UserHomepage />}></Route>
+      <Route path="/explore" element={<Explore />}></Route>
+      <Route path="/messages" element={<Messages />}></Route>
+      <Route path="/profile" element={<Profile />}></Route>
+      <Route path="/more" element={<More />}></Route>
     </Routes>
   );
 }
