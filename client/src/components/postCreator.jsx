a0e2bbb0609c9bccf39d9168ba5abb9b51f59087
Vreij <vreijlal@gmail.com>
2023-04-10 13:58:15 +0300

    post creator component styling added

--- client/src/components/postCreator.jsx
+++ client/src/components/postCreator.jsx
@@ -4,72 +4,8 @@ import { storage } from '../firebase';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { v4 } from 'uuid';
 import "../styles/PostsCreator.scss"
-/*
-function PostCreator() {
-
-   const uploadImage =  async () =>{
-     if(uploadedImage === null){
-      alert("no image found")
-       return;
-     }
-     else{
-       const imageRef = ref(storage, `images/${uploadedImage.name + v4()}`)
-       uploadBytes(imageRef, uploadedImage).then(async () => {
-         alert("img uploaded")
-        await getDownloadURL(imageRef).then( async(url) => { setImageUrl(url)});
-       })
-     }
-   };
-
-  const createPost = async () => {
-    await uploadImage();
-    axios
-      .post("http://localhost:8080/auth/verify", {
-        token: localStorage.getItem("token"),
-      })
-      .then((data) => {
-         axios
-          .post("http://localhost:8080/posts", {
-            user: data.data.name,
-            desc: description,
-            image: imageUrl,
-          })
-          .then(alert("post created!"));
-      });
-
-  };
-  //post description text state
-  let [description, setDescription] = useState("");
-  
-
-  //firebase initialization 
-  //post image upload state
-  let [uploadedImage, setUploadedImage] = useState(null);
-
-   //store img url
-   let [imageUrl, setImageUrl] = useState(null);
-
-  useEffect(() => {
-    console.log("url changed: "  + imageUrl);
-  }, [imageUrl])
-
-  return (
-    <div>
-      <h1>create a post</h1>
-      <input
-        type="text"
-        placeholder="What's on your mind"
-        onChange={(e) => setDescription(e.target.value)}
-      />
-      <input type="file" onChange={(event) => setUploadedImage(event.target.files[0])}/>
-      <button onClick={createPost}>Post</button>
-    </div>
-  );
-}
-*/
-
-////////////////////////////////////
-
+//another test
+import uploadImagePlaceholder from "../uploadImgPlaceholder.jpg";
 
 function PostCreator() {
    //post description text state
@@ -100,6 +36,20 @@ function PostCreator() {
 
  };
 
+ //test
+ const [selectedFile, setSelectedFile] = useState(null);
+ const handleFileChange = (event) => {
+  setUploadedImage(event)
+  const file = event;
+  setSelectedFile(file);
+
+  // Create a thumbnail URL
+  const reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onloadend = () => {
+    document.getElementById('thumbnail').style.backgroundImage = `url(${reader.result})`;
+  };
+};
 
  useEffect( () => {
   if(uploadedImage != null){
@@ -110,26 +60,32 @@ function PostCreator() {
     })
   }
   else{
-
     return;
   }
 
  }, [uploadedImage])
 
+ //another test
+ const handleTextareaChange =  ( async (event) => {
+  const res = await event;
+  setDescription(res)
+ } ) 
+
  return (
    <section className="post-creator-container">
-    <heading>
-      <h4 className="heading-4">create a post</h4>
-    </heading>
+    
+    <header>
+      <h4 className="heading-4">Create A Post</h4>
+    </header>
+    
+
+    <section className="file-input">
+      <input type="file" onChange={(event) => handleFileChange(event.target.files[0])}/>
+      <div id="thumbnail"  style={{ backgroundImage: `url(${uploadImagePlaceholder})` }}></div>
+    </section>
+  
      
-     <input
-       type="text"
-       placeholder="What's on your mind"
-       onChange={(e) => setDescription(e.target.value)}
-     />
-
-     <input type="file" onChange={(event) => setUploadedImage(event.target.files[0])}/>
-
+     <textarea class="comment-section" placeholder="What's on your mind?" onChange={(e) => handleTextareaChange(e.target.value)}></textarea>
 
      <button className="button-1" onClick={createPost}>Post</button>
 
