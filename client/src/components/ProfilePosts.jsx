173361eabfdbf7bc353eb7634da73f5e7059ec7c
Vreij <vreijlal@gmail.com>
2023-04-29 16:36:23 +0300

    profile navbar userposts section functionality added

--- client/src/components/ProfilePosts.jsx
+++ client/src/components/ProfilePosts.jsx
@@ -1,12 +1,53 @@
-import React from 'react'
-import "../styles/ProfilePosts.scss";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import "../styles/Posts.scss";
+import logo from "../Logo.png";
 function ProfilePosts() {
+  let [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    getPosts();
+  }, [posts]);
+
+  const getPosts = () => {
+    axios.get("http://localhost:8080/posts/getposts").then((d) => {
+      setPosts(d.data);
+    });
+  };
+
+  let [username, setUsername] = useState("");
+  useEffect(() => {
+    axios
+      .post("http://localhost:8080/auth/verify", {
+        token: localStorage.getItem("token"),
+      })
+      .then((data) => {
+        setUsername(data.data.name);
+      });
+  }, []);
+
   return (
-    <section className="profile-posts">
-    <h3 className="heading-3">Posts</h3>
-    <p>Content for posts.</p>
-  </section>
-  )
+    <section className="post-wrapper">
+      {posts.map((e) => {
+        if(e.user == username){
+          return (
+            <section className="post-container">
+  
+  
+              <section className="post-img">
+                <img src={e.image} />
+              </section>
+  
+              <section className="post-desc">
+                <p>{e.desc}</p>
+              </section>
+  
+            </section>
+          );
+        }
+      })}
+    </section>
+  );
 }
 
 export default ProfilePosts;
